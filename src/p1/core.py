905f1d1f397be0e0d2551d548ed4cde7a9f12c3b
João Canavarro <jvcanavarro@gmail.com>
2021-04-05 15:26:12 -0300

    Finish raw impl

--- src/p1/core.py
+++ src/p1/core.py
@@ -1,8 +1,28 @@
 from person import Person
 from system import System
 
+aian = Person("aianshay@gmail.com", "Aian", "Shay", "Belém", "UFPA",
+              ["Python", "Data Science", "Machine Learning"],
+              ['Student at xxxx', 'Temp at yyyy', 'Fellow at zzzz'])
+joao = Person("jvcanavarro@gmail.com", "Joao", "Canavarro", "Belém", "UFPA",
+              ["Python", "NLP", "Speech Recognition"],
+              ['Student at xxxx', 'Temp at yyyy', 'Fellow at zzzz'])
+pedro = Person("ppvitor@gmail.com", "Pedro", "Melo", "Belém", "UFPA",
+               ["Java", "Design", "SQL"],
+               ['Student at xxxx', 'Temp at yyyy', 'Fellow at zzzz'])
+isabela = Person("isa.maues@gmail.com", "Isabela", "Maues", "Belém", "UFPA",
+                 ["C++", "Design", "Figma"],
+                 ['Student at xxxx', 'Temp at yyyy', 'Fellow at zzzz'])
 
-system = System()
-aian = Person("aianshay@gmail.com", "Aian", "Shay", "Belém", "UFPA", ["Python", "Data Science", "Machine Learning"], ['Student at xxxx', 'Temp at yyyy', 'Fellow at zzzz'])
-joao = Person("jvcanavarro@gmail.com", "Joao", "Canavarro", "Belém", "UFPA", ["Python", "NLP", "Speech Recognition"], ['Student at xxxx', 'Temp at yyyy', 'Fellow at zzzz'])
-pedro = Person("ppvitor@gmail.com", "Pedro", "Melo", "Belém", "UFPA", ["Java", "Design", "SQL"], ['Student at xxxx', 'Temp at yyyy', 'Fellow at zzzz'])
+system = System(users=[aian, joao, pedro])
+
+system.add_user(user=isabela)
+system.add_xp_to_user(email="ppvitor@gmail.com", experience="Developer at w")
+
+system.get_user_by_email(email="jvcanavarro@gmail.com")
+system.get_user_by_education(education="UFPA")
+
+system.get_xp_by_email(email="aianshay@gmail.com")
+system.get_xp_by_residence(residence="Belém")
+
+system.show_users()
