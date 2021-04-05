a86f863eb854fae5b9ede52358817ff3cb1e140b
João Canavarro <jvcanavarro@gmail.com>
2021-04-05 20:54:55 -0300

    Add json parameters and conversion

--- src/p1/person.py
+++ src/p1/person.py
@@ -1,13 +1,27 @@
+import json
+
+
 class Person:
-    def __init__(self, email: str, name: str, lname: str, residence: str,
-                 education: str, skills: list[str], experience: list[str]):
-        self.email = email
-        self.name = name
-        self.lname = lname
-        self.residence = residence
-        self.education = education
-        self.skills = skills
-        self.experience = experience
+    def __init__(self,
+                 email: str = '',
+                 name: str = '',
+                 lname: str = '',
+                 residence: str = '',
+                 education: str = '',
+                 skills: list[str] = [],
+                 experience: list[str] = [],
+                 serie: str = '',
+                 from_json: bool = False):
+        if from_json:
+            self.__dict__ = json.loads(serie)
+        else:
+            self.email = email
+            self.name = name
+            self.lname = lname
+            self.residence = residence
+            self.education = education
+            self.skills = skills
+            self.experience = experience
 
     def get_email(self) -> str:
         return self.email
@@ -50,6 +64,9 @@ class Person:
 
     def add_experience(self, experience: str):
         self.experience.append(experience)
+
+    def to_json(self):
+        return json.dumps(self, default=lambda x: x.__dict__, indent=2)
 
     def __str__(self):
         return f"Email: {self.email}, First Name: {self.name}, Last Name: {self.lname}, Residence: {self.residence}, " + f"Education: {self.education}, \nSkills: {self.skills}, Experience: {self.experience}.\n"
