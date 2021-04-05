93937a2453d4951ab795c06ae750150a3754161c
João Canavarro <jvcanavarro@gmail.com>
2021-04-05 14:50:58 -0300

    Add mock users to core

---
+++ src/p1/core.py
@@ -0,0 +1,8 @@
+from person import Person
+from system import System
+
+
+system = System()
+aian = Person("aianshay@gmail.com", "Aian", "Shay", "Belém", "UFPA", ["Python", "Data Science", "Machine Learning"], ['Student at xxxx', 'Temp at yyyy', 'Fellow at zzzz'])
+joao = Person("jvcanavarro@gmail.com", "Joao", "Canavarro", "Belém", "UFPA", ["Python", "NLP", "Speech Recognition"], ['Student at xxxx', 'Temp at yyyy', 'Fellow at zzzz'])
+pedro = Person("ppvitor@gmail.com", "Pedro", "Melo", "Belém", "UFPA", ["Java", "Design", "SQL"], ['Student at xxxx', 'Temp at yyyy', 'Fellow at zzzz'])
