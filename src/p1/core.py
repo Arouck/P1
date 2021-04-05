f4ebf1d2f0e98ceb6844e83ab8c5a7438b6ae89f
João Canavarro <jvcanavarro@gmail.com>
2021-04-05 20:55:16 -0300

    Finish pyro4 core impl, missing time tracking and plots

--- src/p1/core.py
+++ src/p1/core.py
@@ -4,31 +4,37 @@ import Pyro4.util
 from person import Person
 from system import System
 
-sys.excepthook = Pyro4.util.excepthook
-system = Pyro4.Proxy("PYRONAME:example.system")
-
-aian = Person("aianshay@gmail.com", "Aian", "Shay", "Belém", "UFPA",
+aian = Person("aianshay@gmail.com", "Aian", "Shay", "Belem", "UFPA",
               ["Python", "Data Science", "Machine Learning"],
               ['Student at xxxx', 'Temp at yyyy', 'Fellow at zzzz'])
-joao = Person("jvcanavarro@gmail.com", "Joao", "Canavarro", "Belém", "UFPA",
+joao = Person("jvcanavarro@gmail.com", "Joao", "Canavarro", "Belem", "UFPA",
               ["Python", "NLP", "Speech Recognition"],
               ['Student at xxxx', 'Temp at yyyy', 'Fellow at zzzz'])
-pedro = Person("ppvitor@gmail.com", "Pedro", "Melo", "Belém", "UFPA",
+pedro = Person("ppvitor@gmail.com", "Pedro", "Melo", "Belem", "UFPA",
                ["Java", "Design", "SQL"],
                ['Student at xxxx', 'Temp at yyyy', 'Fellow at zzzz'])
-isabela = Person("isa.maues@gmail.com", "Isabela", "Maues", "Belém", "UFPA",
+isabela = Person("isa.maues@gmail.com", "Isabela", "Maues", "Belem", "UFPA",
                  ["C++", "Design", "Figma"],
                  ['Student at xxxx', 'Temp at yyyy', 'Fellow at zzzz'])
 
-# system = System(users=[aian, joao, pedro])
+sys.excepthook = Pyro4.util.excepthook
+system = Pyro4.Proxy("PYRONAME:system.core")
 
-system.add_user(user=isabela)
+system.add_user(user=aian.to_json())
+system.add_user(user=isabela.to_json())
+system.add_user(user=joao.to_json())
+system.add_user(user=pedro.to_json())
+
 system.add_xp_to_user(email="ppvitor@gmail.com", experience="Developer at w")
 
-system.get_user_by_email(email="jvcanavarro@gmail.com")
-system.get_user_by_education(education="UFPA")
+print(system.get_user_by_email(email="jvcanavarro@gmail.com"))
+print(system.get_user_by_education(education="UFPA"))
 
-system.get_xp_by_email(email="aianshay@gmail.com")
-system.get_xp_by_residence(residence="Belém")
+print(system.get_xp_by_email(email="aianshay@gmail.com"))
+print(system.get_xp_by_residence(residence="Belem"))
 
+print(*system.users, sep='\n')
+
+# Server Side
 system.show_users()
+system.clean_cache()
